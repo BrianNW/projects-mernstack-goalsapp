51187f16ab67d28376442892949dca63a8dac32a
Brian <briantechdom@gmail.com>
2022-02-24 08:45:18 -0800

    Added api/goals route to test with Postman

--- backend/server.js
+++ backend/server.js
@@ -4,4 +4,9 @@ const port = process.env.PORT || 5000
 
 const app = express()
 
+// 1st argument for url, 2nd argument is a function
+app.get('/api/goals',(req,res) => {
+    res.send('Get goals')
+} )
+
 app.listen(port, () => console.log(`server on port ${port}`))
