9bcfbbc87ecf0decde106ea571e55267750a6176
Brian <briantechdom@gmail.com>
2022-02-27 12:52:58 -0800

    Setup and added first route

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,11 @@ const port = process.env.PORT || 5000
 const app = express()
 
 // 1st argument for url, 2nd argument is a function
-app.get('/api/goals',(req,res) => {
-
-    res.json({message: 'Get goals'})
+// app.get('/api/goals',(req,res) => {
+    // res.json({message: 'Get goals'})
     // res.send('Get goals')
-} )
+// } )
+
+app.use('/api/goals', require('./routes/goalRoutes'))
 
 app.listen(port, () => console.log(`server on port ${port}`))
