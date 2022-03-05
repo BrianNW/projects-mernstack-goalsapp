1bfc7f82115fff1f4401f57553e84a4ada833a0b
Brian <briantechdom@gmail.com>
2022-03-05 15:26:18 -0800

    Updated POST route

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -4,6 +4,14 @@
 const getGoals = (req, res) => {
      res.status(200).json({message: 'Get goals'})   
 }
+
+// @desc    Set Goal
+// @route   POST /api/goals
+// @access  Private
+const setGoal = (req, res) => {
+    res.status(200).json({message: 'Set goals'})   
+}
+
 module.exports = {
     getGoals,
 }
