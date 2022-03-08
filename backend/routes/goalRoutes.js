ad7efa716594b2f36a8f50ddf4600dfa9d8dd5e5
Brian <briantechdom@gmail.com>
2022-03-08 12:03:00 -0800

    Added shorthand router for get and post routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = express.Router()
 const {getGoals, setGoal, updateGoal, deleteGoal} = require('../controllers/goalController')
 
+// shorthand for .get and .post routes
+// router.route('/').get(getGoals).post(setGoal)
+
 //all you need for the .get is the /, as api/goals is already specified in server.js file
 //GET A GOAL
 router.get('/', getGoals)
