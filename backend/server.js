f0f73fcbc734e5c383d661feb39638012b00c008
Brian <briantechdom@gmail.com>
2022-03-10 06:51:14 -0800

    Add urlextended middleware and check for body text

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,10 @@ const port = process.env.PORT || 5000
 
 const app = express()
 
+//add middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
 // 1st argument for url, 2nd argument is a function
 // app.get('/api/goals',(req,res) => {
     // res.json({message: 'Get goals'})
