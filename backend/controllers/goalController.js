a18f716230ad38eb5931d51bd28ecabc9df9d0f4
Brian <briantechdom@gmail.com>
2022-03-13 15:17:45 -0700

    added async for rest route functions

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,14 +1,14 @@
 // @desc    Get Goals
 // @route   GET /api/goals
 // @access  Private
-const getGoals = (req, res) => {
+const getGoals = async (req, res) => {
      res.status(200).json({message: 'Get goals'})   
 }
 
 // @desc    Set Goal
 // @route   POST /api/goals
 // @access  Private
-const setGoal = (req, res) => {
+const setGoal = async (req, res) => {
     //Check if any texts exist
     if(!req.body.text) {
         throw new Error('Please add a text field')
@@ -21,7 +21,7 @@ const setGoal = (req, res) => {
 // @desc    Update Goal
 // @route   PUT /api/goals/:id
 // @access  Private
-const updateGoal = (req, res) => {
+const updateGoal = async (req, res) => {
     res.status(200).json({message: `Update goal ${req.params.id}`})
     
 }
@@ -29,7 +29,7 @@ const updateGoal = (req, res) => {
 // @desc    Delete Goal
 // @route   DELETE /api/goals/:id
 // @access  Private
-const deleteGoal = (req, res) => {
+const deleteGoal = async (req, res) => {
     res.status(200).json({message: `Deleted goal ${req.params.id}`})
     
 }
