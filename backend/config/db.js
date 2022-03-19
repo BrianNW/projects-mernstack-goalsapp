730de9264d517ab94a3dc55e979093af10cb616a
Brian <briantechdom@gmail.com>
2022-03-19 15:04:17 -0700

    Added process comments

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose')
 
+// set up try catch async function
 const connectDB = async () => {
     try {
+        
+        //contain await connection 
         const conn = await mongoose.connect(process.env.MONGO_URI)
 
+        // if successful
         console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+
     }catch(error) {
         console.log(error);
         // close the process and pass the failure as 1
