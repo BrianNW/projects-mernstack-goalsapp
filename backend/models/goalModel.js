26c7f53cabdd67f3aa284e5eda59b3670430263e
Brian <briantechdom@gmail.com>
2022-04-04 11:45:20 -0700

    Add user type and reference it to userModel.js

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose')
 
-const goalSchema = mongoose.Schema({
+const goalSchema = mongoose.Schema(
+{
+    // We want this user type to be an object id, and ref(reference) the User model.
+    // This will allow us to have a user associated with a goal
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
     // Object with type of string
     text: {
         type: String,
