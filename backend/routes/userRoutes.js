f97b4dc522c0b0c90298fe852562d3e74360f24a
Brian <briantechdom@gmail.com>
2022-04-09 15:53:11 -0700

    Added loginUser and getMe controllers

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { registerUser } = require('../controllers/userController')
+const { registerUser, loginUser, getMe } = require('../controllers/userController')
 
 // When you make a post request to /api/users, we're adding a user here
 router.post('/', registerUser)
+router.post('/login', loginUser)
+router.post('/me', getMe)
 
 module.exports = router
