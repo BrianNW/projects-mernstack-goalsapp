d200d42a784480218fbaae1c47c68f1c2db216bf
Brian <briantechdom@gmail.com>
2022-04-09 15:49:20 -0700

    Added auth user and get user data controllers

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,4 +6,18 @@ const registerUser = (req, res) => {
     res.json({message: 'Register User'})
 }
 
-module.exports = { registerUser }
+// @desc    Authenticate a user
+// @route   POST api/users/login
+// @access  Public
+const loginUser = (req, res) => {
+    res.json({message: 'Login User'})
+}
+
+// @desc    Get user data
+// @route   GET api/users/me
+// @access  Public
+const getMe = (req, res) => {
+    res.json({message: 'Display user data'})
+}
+
+module.exports = { registerUser, loginUser,getMe }
