aa308880d3c76efb17b6f010b1549b351f7d8544
Brian <briantechdom@gmail.com>
2022-04-14 11:18:12 -0700

    Wrap user controller functions in async and and async handlers to handle exceptions

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,26 +1,30 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
 
 // @desc    Register new user
 // @route   POST api/users/:id
 // @access  Public
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
     // test with json and Postman
     res.json({message: 'Register User'})
-}
+})
 
 // @desc    Authenticate a user
 // @route   POST api/users/login
 // @access  Public
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
     // test with json and Postman
     res.json({message: 'Login User'})
-}
+})
 
 // @desc    Get user data
 // @route   GET api/users/me
 // @access  Public
-const getMe = (req, res) => {
+const getMe = asyncHandler(async (req, res) => {
     // test with json and Postman
     res.json({message: 'Display user data'})
-}
+})
 
 module.exports = { registerUser, loginUser,getMe }
