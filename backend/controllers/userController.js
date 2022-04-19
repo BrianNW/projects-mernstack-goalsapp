d60180b3e6c2ff26aeb709b76e0195ceb1f016f1
Brian <briantechdom@gmail.com>
2022-04-18 18:46:49 -0700

    Add a generate JWT parameter for new user creation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -43,7 +43,8 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(201).json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._Id)
         })
     } else {
         res.status(400)
