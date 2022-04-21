3902df51b8a117cd2756e36eb47f765e3c2d87c1
Brian <briantechdom@gmail.com>
2022-04-21 12:07:44 -0700

    add async function and exported

--- backend/middleware/authmiddleware.js
+++ backend/middleware/authmiddleware.js
@@ -2,3 +2,8 @@ const jwt = require('jsonwebtoken')
 const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
 
+const protect = asyncHandler(async(req, res, next) => {
+
+})
+
+module.exports = { protect }
