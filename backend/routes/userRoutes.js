62030d2248b2230bfb73697f2a1b238c1927d70c
Brian <briantechdom@gmail.com>
2022-04-22 15:51:30 -0700

    Add middleware route protection

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const { registerUser, loginUser, getMe, } = require('../controllers/userController')
+const {protect} =  require('../middleware/authMiddleware')
 
 // When you make a post request to /api/users, we're adding a user here
 router.post('/', registerUser)
 router.post('/login', loginUser)
-router.get('/me', getMe)
+router.get('/me', protect, getMe)
 
 module.exports = router
