d69cfd2330f15a6a90783714ee5b257a11ae4884
Brian <briantechdom@gmail.com>
2022-04-28 15:45:51 -0700

    Add the protect middleware

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,20 +2,24 @@ const express = require('express')
 const router = express.Router()
 const {getGoals, setGoal, updateGoal, deleteGoal} = require('../controllers/goalController')
 
+// protect the goal routes
+const {protect} = require('../middleware/authMiddleware')
+
 // shorthand for .get and .post routes
 // router.route('/').get(getGoals).post(setGoal)
 
 //all you need for the .get is the /, as api/goals is already specified in server.js file
 //GET A GOAL
-router.get('/', getGoals)
-
+// router.get('/', getGoals)
 //CREATE A GOAL
-router.post('/', setGoal)
-
+// router.post('/', setGoal)
 //UPDATE A GOAL
-router.put('/:id', updateGoal)
-
+// router.put('/:id', updateGoal)
 //DELETE A GOAL
-router.delete('/:id',deleteGoal)
+// router.delete('/:id',deleteGoal)
+
+// add protect as an argument
+router.route('/').get(protect, getGoals).post(setGoal)
+router.route('/:id').delete(deleteGoal).put(updateGoal)
 
 module.exports = router
