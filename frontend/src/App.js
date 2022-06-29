3536f455077481f1512fc17883832d782b9298df
Brian <briantechdom@gmail.com>
2022-06-28 17:23:46 -0700

    Add Register Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
         <Routes>
 
           <Route path="/" element={<Dashboard />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login />} />          
+          <Route path="/register" element={<Register />} />
         </Routes>
       
       </div>
