2bdb981c45623b3dded3388a131bade71c4c0508
Brian <briantechdom@gmail.com>
2022-06-28 17:22:47 -0700

    Add Dashboard Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <>
     <Router>
       <div className="container">
-        <Routes></Routes>
+        <Routes>
+
+          <Route path="/" element={<Dashboard />} />
+        </Routes>
       
       </div>
     </Router>
