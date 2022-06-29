67d07b2b9f7c7b6bbd39a7ff36dfa95b8c0902e1
Brian <briantechdom@gmail.com>
2022-06-28 17:23:19 -0700

    Add Login Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
 
           <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       
       </div>
