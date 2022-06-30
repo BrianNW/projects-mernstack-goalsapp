274effafd26aedd1c98316abe9d745414006f656
Brian <briantechdom@gmail.com>
2022-06-30 15:52:45 -0700

    Import Header and add inside Router tag

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,18 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Header from './components/Header'
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
+// With react Router 6, make sure there no other elements within the RouteS tag unless it's a RoutE tag.
 function App() {
   return (
     <>
     <Router>
-      <div className="container">
+      <div className="container">        
+        <Header />
         <Routes>
-
+          
           <Route path="/" element={<Dashboard />} />
           <Route path="/login" element={<Login />} />          
           <Route path="/register" element={<Register />} />
