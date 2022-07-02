8d1043ee1039ce839766aab11b91789110c361bb
Brian <briantechdom@gmail.com>
2022-07-02 14:09:17 -0700

    Setup useState

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,15 @@
 import {useState, useEffect} from 'react'
 
 function Register() {
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+
+  })
+
   return (
     <div>Register</div>
   )
