b605690045768027044620c4ea10829a0b919f39
Brian <briantechdom@gmail.com>
2022-09-01 11:41:00 -0700

    Add Login code

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,98 @@
+import React, { Component }  from 'react'
+import {useState, useEffect} from 'react'
+import {FaSignInAlt} from 'react-icons/fa'
+
 function Login() {
+  // setup useState
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+
+  })
+
+  // Destructure form data fields
+  const {name, email, password, password2} = formData;
+
+  // Add onChange function for input below
+  // This allows you to type into the text fields
+  const onChange = (e) => {
+    // set form data to an object
+    setFormData((prevState) => ({
+      // spread operator across the previous state, as we want the other fields
+      ...prevState,
+      //get the key by whatever the name value is
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>Login</div>
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Please create an account</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit="{onSubmit}">
+          <div className="form-group">
+            <input 
+              type="text" 
+              className="form-control" 
+              id="name" 
+              name="name" 
+              value={name} 
+              placeholder="Enter your name" onChange={onChange}
+            />     
+          </div>
+
+          <div className="form-group">
+            <input 
+              type="email" 
+              className="form-control" 
+              id="email" 
+              name="email" 
+              value={email} 
+              placeholder="Enter your email" onChange={onChange}
+            />     
+          </div>
+
+          <div className="form-group">
+            <input 
+              type="password" 
+              className="form-control" 
+              id="password" 
+              name="password" 
+              value={password} 
+              placeholder="Enter your password" onChange={onChange}
+            />     
+          </div>
+
+          <div className="form-group">
+            <input 
+              type="password" 
+              className="form-control" 
+              id="password2" 
+              name="password2" 
+              value={password} 
+              placeholder="Confirm your password" onChange={onChange}
+            />  
+
+            <div className="form-group">
+              <button type="submit" class="btn btn-block">Submit</button>
+            </div>   
+          </div>
+            
+        </form>
+      </section>
+    </>
   )
 }
 
