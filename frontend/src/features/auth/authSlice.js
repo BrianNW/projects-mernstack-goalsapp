c88961ed84bb0490c134b638917a9b26349f83d8
Brian <briantechdom@gmail.com>
2022-09-04 18:49:56 -0700

    Create initial state of the app as an object

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,1 +1,10 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+
+// setup object
+const initialState = {
+    user: null,
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: ''
+}
