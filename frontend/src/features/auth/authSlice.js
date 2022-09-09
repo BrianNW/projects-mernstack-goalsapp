6d912b525799de95f3fb65e61724242210c30557
Brian <briantechdom@gmail.com>
2022-09-09 16:02:53 -0700

    Setup user to store localStorage item in

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,5 +1,11 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
 
+// Grab JWT and save to local storage
+
+// Get user from localStorage. Use .parse to read from JSON string to object
+const user = JSON.parse()
+
+
 // setup object
 const initialState = {
     user: null,
